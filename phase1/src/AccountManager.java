60fef3009bbfc315db42976a4ccd221a52c4d989
cycv5 <charlestsai0919@gmail.com>
2020-10-22 20:32:08 -0400

    Added AccountManager.java.

---
+++ phase1/src/AccountManager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An Account Manager. A use case class for managing user's account.
+ */
+
+public class AccountManager {
+}
