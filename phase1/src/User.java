2c1c834fb74c6fd11f59b665563c9908b9dea76a
Colin Imaizumi-Rankin <cimaizumirankin@gmail.com>
2020-10-27 16:19:49 -0700

    Implemented User.

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,17 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String password;
+    private String username;
+    private boolean logInStatus;
+    private List<User> messageable;
+    private List<Event> eventsAttending;
+
     public void setPassword(String newPassword) {
-    }
-    public void setUsername(String username) {
-    }
-    public void setLogInStatus(boolean b) {
+        this.password = newPassword;
     }
 
     public String getPassword() {
-        return "";
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getUsername() {
-        return "";
+        return username;
+    }
+
+    public void setLogInStatus(boolean b) {
+        this.logInStatus = b;
+    }
+
+    public boolean getLoginStatus() {
+        return logInStatus;
+    }
+
+    public List<Object> getMessages() {
+        return new ArrayList<>();
+    }
+
+    public void setMessageable(List<User> users) {
+        messageable = users;
+    }
+
+    public void addMessageable(User u) {
+        messageable.add(u);
+    }
+
+    public List<User> getMessageable() {
+        return messageable;
+    }
+
+    public List<Event> getEventsAttending() {
+        return eventsAttending;
+    }
+
+    public void setEventsAttending(List<Event> events) {
+        eventsAttending = events;
+    }
+
+    public void addEventAttending(Event e) {
+        eventsAttending.add(e);
     }
 
 }
