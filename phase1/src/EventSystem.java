b02750bb0507912b95f257bd2d563532d55a5815
raineakiyama-chen <#bacongriffin1bjj>
2020-10-28 19:29:57 -0400

    Added basic template for the EventSystem controller class.

---
+++ phase1/src/EventSystem.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class EventSystem {
+
+    private AccountManager am = new AccountManager();
+    private EventManager em = new EventManager();
+    /** Add MessageManager when ready. */
+    private String currentUser;
+
+    public void run() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void logIn(String username, String password) {
+        if (am.logInUser(username, password)) {
+            currentUser = username;
+        } else {
+            System.out.println("Sorry wrong username or password, please try again.");
+        }
+    }
+
+    public void signOut() {}
+
+    public void createAccount(String username, String password) {}
+
+    public void changePassword(String password) {}
+
+    public void getListOfEvents() {}
+
+    public void attendEvent(String eventName) {}
+
+    public void cancelAttendEvent(String eventName) {}
+
+    public void sendMessage(String message, String recipient) {}
+
+    public void seeMessages() {}
+
+}
