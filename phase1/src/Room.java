79360a3cefb3522431ffea073cf9a788f7e3e5ba
dingyouy <daisy.ding@mail.utoronto.ca>
2020-10-28 16:28:38 +0800

    daisy

--- phase1/src/Room.java
+++ phase1/src/Room.java
@@ -1,2 +1,32 @@
+import java.util.List;
+
 public class Room {
+    private int roomNumber;
+    private int roomCapacity;
+    private List<Event> roomSchedule;
+
+    public void setRoomNumber(int num){
+        this.roomNumber=num;
+    }
+    public int getRoomNumber(){
+        return roomNumber;
+    }
+
+    public void setRoomCapacity(int num){
+        this.roomCapacity=num;
+    }
+    public int getRoomCapacity(){
+        return roomCapacity;
+    }
+
+    public void setRoomSchedule(List<Event> s){
+        this.roomSchedule=s;
+    }
+    public List<Event> getRoomSchedule(){
+        return roomSchedule;
+    }
+
+
 }
+
+
